290725acd77f383e554b3db52f287fa81c23d7b1
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-31 10:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let size = document.getElementById("sizeInfo");
+
+window.addEventListener("resize", () => {
+	let x = window.innerWidth;
+	let y = window.innerHeight;
+	size.textContent = `Width: ${x} and Height: ${y}`
+})
